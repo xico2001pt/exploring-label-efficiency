cf0eb48f8eb34325ce09e2bd7b13f9ef6c5e93ba
Francisco Cerqueira <up201905337@fe.up.pt>
2024-01-10 13:13:21 +0000

    feat: admetrics support

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,10 +1,12 @@
 from .losses import CrossEntropyLoss
+from torchmetrics import Accuracy
 from torch.optim import Adam, SGD
 from torch.optim.lr_scheduler import StepLR
 from .stop_conditions import StopPatience
 
 classes = {
     "losses": [CrossEntropyLoss],
+    "metrics": [Accuracy],
     "optimizers": [Adam, SGD],
     "schedulers": [StepLR],
     "stop_conditions": [StopPatience]
