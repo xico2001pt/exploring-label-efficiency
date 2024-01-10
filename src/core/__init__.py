00af56d4ab7e29dfa5022fb772fd8d9282c3e867
Francisco Cerqueira <up201905337@fe.up.pt>
2024-01-10 14:53:57 +0000

    feat: start adding configuration files

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,13 +1,13 @@
 from .losses import CrossEntropyLoss
 from torchmetrics import Accuracy
 from torch.optim import Adam, SGD
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import ExponentialLR, MultiStepLR
 from .stop_conditions import StopPatience
 
 classes = {
-    "losses": [CrossEntropyLoss],
-    "metrics": [Accuracy],
-    "optimizers": [Adam, SGD],
-    "schedulers": [StepLR],
-    "stop_conditions": [StopPatience]
+    "losses": [CrossEntropyLoss],  # Add the loss classes here
+    "metrics": [Accuracy],  # Add the metric classes here
+    "optimizers": [Adam, SGD],  # Add the optimizer classes here
+    "schedulers": [ExponentialLR, MultiStepLR],  # Add the scheduler classes here
+    "stop_conditions": [StopPatience]  # Add the stop condition classes here
 }
