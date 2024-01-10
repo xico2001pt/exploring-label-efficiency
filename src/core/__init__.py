4c0f537f7cf7340bff1b2a90426c9d7bef112404
Francisco Cerqueira <up201905337@fe.up.pt>
2024-01-10 13:02:36 +0000

    feat: add optimizers and schedulers support

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,7 +1,11 @@
 from .losses import CrossEntropyLoss
+from torch.optim import Adam, SGD
+from torch.optim.lr_scheduler import StepLR
 from .stop_conditions import StopPatience
 
 classes = {
     "losses": [CrossEntropyLoss],
+    "optimizers": [Adam, SGD],
+    "schedulers": [StepLR],
     "stop_conditions": [StopPatience]
 }
