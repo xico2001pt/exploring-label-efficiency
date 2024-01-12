fb249f7e625ffa49ca1e8995dfc1db6206e5314a
Francisco Cerqueira <up201905337@fe.up.pt>
2024-01-12 18:50:45 +0000

    chore: add experiment with top-5 accuracy

--- src/tools/train.py
+++ src/tools/train.py
@@ -80,7 +80,7 @@ def main(args):
     config_name = _get_config_name(loader, args.config)
 
     log_dir = os.path.join(LOGS_DIR, config_name)
-    logger = Logger(log_dir, console_output=True, output_filename=c.Logging.TRAIN_OUTPUT_FILENAME)
+    logger = Logger(log_dir, console_output=False, output_filename=c.Logging.TRAIN_OUTPUT_FILENAME)
 
     try:
         logger.info("Loading configuration files...")
