3a8233d690f4b66910ecc6e6da706c5694885df6
Francisco Cerqueira <up201905337@fe.up.pt>
2024-01-22 16:44:01 +0000

    feat: add all experiments

--- src/methods/semisl/pi_model.py
+++ src/methods/semisl/pi_model.py
@@ -14,7 +14,7 @@ AUGMENTATIONS = {
 class PiModel(SemiSLMethod):
     def __init__(self, max_unsupervised_weight, unsupervised_weight_rampup_length, augmentations):
         self.supervised_loss = CrossEntropyLoss(reduction='mean')
-        self.unsupervised_loss = MSELoss(reduction='sum')
+        self.unsupervised_loss = MSELoss(reduction='mean')
         self.max_unsupervised_weight = max_unsupervised_weight
         self.unsupervised_weight_fn = exp_rampup(unsupervised_weight_rampup_length)
         self.augmentations = [AUGMENTATIONS[augmentation] for augmentation in augmentations]
@@ -23,32 +23,34 @@ class PiModel(SemiSLMethod):
         return False
 
     def on_change_epoch(self, epoch):
+        epoch = epoch - 1
         self.unsupervised_weight = self.unsupervised_weight_fn(epoch) * self.max_unsupervised_weight if epoch > 0 else 0.0
 
     def compute_loss(self, labeled, targets, unlabeled):
-        if unlabeled is not None:
-            p1, p2 = self.compute_predictions(unlabeled)
-            unsupervised_loss_1 = self.unsupervised_loss(p1, p2)
-            unlabeled_size = unlabeled.size(0)
+        if labeled is not None:
+            l1, l2 = self.augment(labeled)
         else:
-            unsupervised_loss_1 = 0.0
-            unlabeled_size = 0
+            l1 = l2 = torch.empty(0).to(unlabeled.device)
+            supervised_loss = 0.0
+            labeled_outputs = None
+
+        if unlabeled is not None:
+            u1, u2 = self.augment(unlabeled)
+        else:
+            u1 = u2 = torch.empty(0).to(labeled.device)
+
+        cat1, cat2 = torch.cat([l1, u1]), torch.cat([l2, u2])
+
+        out1 = self.model(cat1)
+
+        with torch.no_grad():
+            out2 = self.model(cat2)
 
         if labeled is not None:
-            p1, p2 = self.compute_predictions(labeled)
-            labeled_outputs = p1
+            labeled_outputs = out1[:labeled.size(0)]
             supervised_loss = self.supervised_loss(labeled_outputs, targets)
-            unsupervised_loss_2 = self.unsupervised_loss(p1, p2)
-            labeled_size = labeled.size(0)
-        else:
-            labeled_outputs = None
-            supervised_loss = 0.0
-            unsupervised_loss_2 = 0.0
-            labeled_size = 0
 
-        total_size = labeled_size + unlabeled_size  # Never zero
-
-        unsupervised_loss = (unsupervised_loss_1 + unsupervised_loss_2) / float(total_size / 10)  # TODO: NUM CLASSES VARIABLE
+        unsupervised_loss = self.unsupervised_loss(out1, out2) / 10  # TODO: NUM CLASSES VARIABLE
         unsupervised_weighted_loss = self.unsupervised_weight * unsupervised_loss
 
         total_loss = supervised_loss + unsupervised_weighted_loss
@@ -71,10 +73,3 @@ class PiModel(SemiSLMethod):
         x_1 = self.stochastic_augmentation(x.clone())
         x_2 = self.stochastic_augmentation(x.clone())
         return x_1, x_2
-
-    def compute_predictions(self, input):
-        aug1, aug2 = self.augment(input)
-        predictions1 = self.model(aug1)
-        with torch.no_grad():
-            predictions2 = self.model(aug2)
-        return predictions1, predictions2
