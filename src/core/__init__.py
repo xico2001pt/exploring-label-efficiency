38cbf61de1b0fe5fea5d2578863bd64edcd03d81
Francisco Cerqueira <up201905337@fe.up.pt>
2024-02-10 18:10:41 +0000

    feat: add segmentation metrics and losses

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,13 +1,13 @@
-from .losses import CrossEntropyLoss
-from torchmetrics import Accuracy
+from .losses import CrossEntropyLoss, DiceLoss
+from torchmetrics import Accuracy, Dice, JaccardIndex
 from torch.optim import Adam, SGD
 from torch.optim.lr_scheduler import ExponentialLR, MultiStepLR
 from .schedulers import ExpWarmupLR
 from .stop_conditions import StopPatience
 
 classes = {
-    "losses": [CrossEntropyLoss],  # Add the loss classes here
-    "metrics": [Accuracy],  # Add the metric classes here
+    "losses": [CrossEntropyLoss, DiceLoss],  # Add the loss classes here
+    "metrics": [Accuracy, Dice, JaccardIndex],  # Add the metric classes here
     "optimizers": [Adam, SGD],  # Add the optimizer classes here
     "schedulers": [ExponentialLR, ExpWarmupLR, MultiStepLR],  # Add the scheduler classes here
     "stop_conditions": [StopPatience]  # Add the stop condition classes here
--- src/core/losses.py
+++ src/core/losses.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torchmetrics
 
 
 class CrossEntropyLoss(nn.CrossEntropyLoss):
@@ -8,3 +9,12 @@ class CrossEntropyLoss(nn.CrossEntropyLoss):
     def forward(self, input, target):
         loss = super().forward(input, target)
         return {"total": loss}
+
+
+class DiceLoss(torchmetrics.Dice):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input, target):
+        loss = super().forward(input, target)
+        return {"total": 1 - loss}
