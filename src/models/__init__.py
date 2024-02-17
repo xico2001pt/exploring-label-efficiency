f63b25b9c0788fb660d5c4e68e63576025baa448
Francisco Cerqueira <up201905337@fe.up.pt>
2024-02-17 12:23:34 +0000

    fix: revert model to DeepLabV3

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
-from .deeplab import DeepLabV3PlusResNet101
-from .resnet import ResNet101
+from .deeplab import DeepLabV3
 from .wideresnet import WideResNet
 
-classes = [DeepLabV3PlusResNet101, ResNet101, WideResNet]  # Add the model classes here
+classes = [DeepLabV3, WideResNet]  # Add the model classes here
