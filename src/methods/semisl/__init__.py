18995b39a34e0f74706c3681e873184ea9b426b1
Francisco Cerqueira <up201905337@fe.up.pt>
2024-03-15 17:16:16 +0000

    feat: add FixMatch algorithm draft

--- src/methods/semisl/__init__.py
+++ src/methods/semisl/__init__.py
@@ -2,6 +2,7 @@ from .pi_model import PiModelCIFAR10, PiModelSVHN, PiModelCityscapesSeg
 from .temporal import TemporalEnsemblingCIFAR10, TemporalEnsemblingSVHN
 from .mixmatch import MixMatchCIFAR10, MixMatchSVHN, MixMatchCityscapesSeg
 from .remixmatch import ReMixMatchCIFAR10, ReMixMatchSVHN
+from .fixmatch import FixMatchCIFAR10
 
 classes = [
     PiModelCIFAR10,
@@ -14,4 +15,5 @@ classes = [
     MixMatchCityscapesSeg,
     ReMixMatchCIFAR10,
     ReMixMatchSVHN,
+    FixMatchCIFAR10,
 ]  # Add the method classes here
---
+++ src/methods/semisl/fixmatch.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+import torchvision.transforms.v2 as v2
+from .semisl_method import SemiSLMethod
+
+
+def default_process_targets(targets, num_classes):
+    return F.one_hot(targets, num_classes).float()
+
+
+class FixMatch(SemiSLMethod):
+    def __init__(self, wu, confidence, labeled_transform, weak_unlabeled_transform, strong_unlabeled_transform, supervised_loss, unsupervised_loss, process_targets=default_process_targets):
+        self.wu = wu
+        self.confidence = confidence
+        self.labeled_transform = labeled_transform
+        self.weak_unlabeled_transform = weak_unlabeled_transform
+        self.strong_unlabeled_transform = strong_unlabeled_transform
+        self.supervised_loss = supervised_loss
+        self.unsupervised_loss = unsupervised_loss
+        if process_targets is None:
+            process_targets = default_process_targets
+        self.process_targets = process_targets
+
+    def on_start_train(self, train_data):
+        self.num_classes = train_data.num_classes
+
+    def compute_loss(self, idx, labeled, targets, unlabeled):
+        labeled, targets, strong_unlabeled, preds = self.pseudo_labelling(labeled, targets, unlabeled)
+
+        inputs = torch.cat([labeled, strong_unlabeled], dim=0)
+        outputs = self.model(inputs)
+        del inputs
+
+        unlabeled_idx = labeled.size(0)
+        labeled_outputs = outputs[:unlabeled_idx]
+        supervised_loss = self.supervised_loss(labeled_outputs, targets)
+
+        strong_unlabeled_outputs = outputs[unlabeled_idx:]
+        preds = torch.cat([labeled_outputs, preds], dim=0)
+
+        # Apply threshold to the predictions
+        preds = preds.detach().softmax(dim=1)
+        max_probs, preds = preds.max(dim=1)
+        mask = max_probs.ge(self.confidence).float()
+
+        unsupervised_loss = self.unsupervised_loss(strong_unlabeled_outputs, preds) * mask
+        unsupervised_loss = unsupervised_loss.mean()
+        unsupervised_weighted_loss = unsupervised_loss * self.wu
+
+        total_loss = supervised_loss + unsupervised_weighted_loss
+
+        loss = {
+            'total': total_loss,
+            'supervised': supervised_loss,
+            'unsupervised': unsupervised_loss,
+            'unsupervised_weighted': unsupervised_weighted_loss,
+        }
+
+        return labeled_outputs, targets.argmax(dim=1), loss
+
+    def pseudo_labelling(self, labeled, targets, unlabeled):
+        targets = self.process_targets(targets, self.num_classes)
+        labeled, targets = self.labeled_transform(labeled, targets)
+
+        with torch.no_grad():
+            weak_unlabeled = self.weak_unlabeled_transform(unlabeled)
+            preds = self.model(weak_unlabeled)
+
+        unlabeled = torch.cat([labeled, unlabeled], dim=0)
+        strong_unlabeled = self.strong_unlabeled_transform(unlabeled)
+
+        return labeled, targets, strong_unlabeled, preds
+
+
+def FixMatchCIFAR10(wu, confidence):
+    labeled_transform = v2.Compose([
+        v2.RandomCrop((32, 32), padding=4, padding_mode='reflect'),
+        v2.RandomHorizontalFlip(),
+    ])
+    weak_unlabeled_transform = v2.Compose([
+        v2.RandomCrop((32, 32), padding=4, padding_mode='reflect'),
+        v2.RandomHorizontalFlip(),
+    ])
+    strong_unlabeled_transform = v2.Compose([
+        v2.RandomCrop((32, 32), padding=4, padding_mode='reflect'),
+        v2.RandomHorizontalFlip(),
+        v2.RandAugment(2, 10),
+    ])
+    supervised_loss = CrossEntropyLoss()
+    unsupervised_loss = CrossEntropyLoss(reduction='none')  # Important not to have reduction here
+    return FixMatch(wu, confidence, labeled_transform, weak_unlabeled_transform, strong_unlabeled_transform, supervised_loss, unsupervised_loss)
